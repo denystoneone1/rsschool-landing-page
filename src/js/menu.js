476d5571201a229eb4000b1db337ff8624ab7d68
Ok4m1 <denystoneone@gmail.com>
2026-09-21 21:28:44 +0500

    feat: add catalog page with product grid

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,8 @@
 import '../scss/menu.scss'
 import { initTheme } from './theme.js'
 import { initBurger } from './burger.js'
+import { initCatalog } from './catalog.js'
 
 initTheme()
 initBurger()
+initCatalog()
