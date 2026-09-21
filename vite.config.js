0f32196b1db3f73ff18a7717a387cd5fc4abb64f
Ok4m1 <denystoneone@gmail.com>
2026-09-22 02:08:26 +0500

    chore: exclude html from prettier formatting

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	// Относительные пути, чтобы сборка работала из подкаталога GitHub Pages.
 	base: './',
 	build: {
 		sourcemap: true,
